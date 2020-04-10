b7b4e583152eb6ad551f90cf3f3d73590c732392
“tongyangsheng” <964997115@qq.com>
2020-04-10 13:59:15 +0800

    版本更新0.05

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,8 @@
 ## 0.0.2
 
 * TODO: fix some Bug,remove default function
+
+
+## 0.0.5
+
+* TODO: Add RewardVideo,reBuild API
--- lib/pangolin.dart
+++ lib/pangolin.dart
@@ -1,40 +1,5 @@
-import 'dart:async';
+library pangolon;
 
-import 'package:flutter/services.dart';
+export 'package:pangolin/pangolin_main.dart';
+export 'package:pangolin/pangolin_response.dart';
 
-class Pangolin {
-  static const MethodChannel _channel =
-      const MethodChannel('com.tongyangsheng.pangolin');
-
-  static Future<bool> register(String appId,bool useTextureView,String appName,bool allowShowNotify,bool allowShowPageWhenScreenLock,bool debug,bool supportMultiProcess) async{
-    assert(appId != Null);
-    return await _channel.invokeMethod("register",
-        {
-          "appId":appId,
-          "useTextureView":useTextureView,
-          "appName":appName,
-          "allowShowNotify":allowShowNotify,
-          "allowShowPageWhenScreenLock":allowShowPageWhenScreenLock,
-          "debug":debug,
-          "supportMultiProcess":supportMultiProcess
-        }
-    );
-  }
-
-  static Future<bool> loadSplashAd(String mCodeId, bool debug) async{
-    return await _channel.invokeMethod("loadSplashAd",
-        {
-          "mCodeId":mCodeId,
-          "debug":debug
-        }
-    );
-  }
-
-  static Future loadExpressAd(bool isHorizontal, String mCodeId) async {
-    return await _channel.invokeMethod("loadExpressAd",
-    {
-      "isHorizontal" : isHorizontal,
-      "mCodeId" : mCodeId
-    });
-  }
-}
