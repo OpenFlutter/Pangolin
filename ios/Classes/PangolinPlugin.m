314e252df4607794ce33d7c989f9d6049dd4ebaa
“tongyangsheng” <964997115@qq.com>
2020-05-21 16:06:38 +0800

    v0.1.1

--- ios/Classes/PangolinPlugin.m
+++ ios/Classes/PangolinPlugin.m
@@ -1,7 +1,11 @@
 #import "PangolinPlugin.h"
 #import <BUAdSDK/BUAdSDK.h>
 
-@interface PangolinPlugin ()<BURewardedVideoAdDelegate>
+@interface PangolinPlugin ()<BUNativeExpressRewardedVideoAdDelegate,BUSplashAdDelegate,
+BUNativeAdsManagerDelegate,BUVideoAdViewDelegate,BUNativeAdDelegate,
+BUNativeExpressAdViewDelegate,
+BUNativeExpressFullscreenVideoAdDelegate,
+BUNativeExpresInterstitialAdDelegate>
 @property (nonatomic, strong) UITextField *playableUrlTextView;
 @property (nonatomic, strong) UITextField *downloadUrlTextView;
 @property (nonatomic, strong) UITextField *deeplinkUrlTextView;
@@ -10,17 +14,26 @@
 @property (nonatomic, assign) BOOL isPlayableUrlValid;
 @property (nonatomic, assign) BOOL isDownloadUrlValid;
 @property (nonatomic, assign) BOOL isDeeplinkUrlValid;
+
 @property (nonatomic, strong) BURewardedVideoAd *rewardedVideoAd;
+@property (nonatomic, strong) BUNativeExpressRewardedVideoAd *rewardedAd;
+
 @end
 
 @implementation PangolinPlugin
+
+FlutterMethodChannel* globalMethodChannel;
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
                                      methodChannelWithName:@"com.tongyangsheng.pangolin"
                                      binaryMessenger:[registrar messenger]];
     PangolinPlugin* instance = [[PangolinPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
+    
+    globalMethodChannel = channel;
 }
+
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     if ([@"register" isEqualToString:call.method]) {
@@ -33,28 +46,78 @@
     }
     else if([@"loadSplashAd" isEqualToString:call.method])
     {
+        NSString* mCodeId = call.arguments[@"mCodeId"];
+        
         [BUAdSDKManager setIsPaidApp:NO];
         [BUAdSDKManager setLoglevel:BUAdSDKLogLevelDebug];
         CGRect frame = [UIScreen mainScreen].bounds;
-        BUSplashAdView *splashView = [[BUSplashAdView alloc] initWithSlotID:@"887310537" frame:frame];
+        BUSplashAdView *splashView = [[BUSplashAdView alloc] initWithSlotID:mCodeId frame:frame];
         splashView.delegate = self;
         UIWindow *keyWindow = [UIApplication sharedApplication].windows.firstObject;
         [splashView loadAdData];
         [keyWindow.rootViewController.view addSubview:splashView];
         splashView.rootViewController = keyWindow.rootViewController;
     }
-    else if([@"loadExpressAd" isEqualToString:call.method])
+    else if([@"loadRewardAd" isEqualToString:call.method])
     {
-//        BURewardedVideoModel *model = [[BURewardedVideoModel alloc] init];
-//        model.userId = @"123";
-//        self.rewardedVideoAd = [[BURewardedVideoAd alloc] initWithSlotID:self.viewModel.slotID rewardedVideoModel:model];
-//        self.rewardedVideoAd.delegate = self;
-//        [self.rewardedVideoAd loadAdData];
+            NSString* slotId = call.arguments[@"mCodeId"];
+            NSString* userId = call.arguments[@"userID"];
+            NSString* rewardName = call.arguments[@"rewardName"];
+            
+            BURewardedVideoModel *model = [[BURewardedVideoModel alloc] init];
+            model.userId = userId;
+            model.rewardName = rewardName;
+        
+            self.rewardedAd = [[BUNativeExpressRewardedVideoAd alloc] initWithSlotID:slotId rewardedVideoModel:model];
+            self.rewardedAd.delegate = self;
+            [self.rewardedAd loadAdData];
+            result(@YES);
     }
     else
     {
         result(FlutterMethodNotImplemented);
     }
+}
+
+//展示视频用
+- (UIViewController *)rootViewController{
+    UIViewController *rootVC = [[UIApplication sharedApplication].delegate window].rootViewController;
+    
+    UIViewController *parent = rootVC;
+    while((parent = rootVC.presentingViewController) != nil){
+        rootVC = parent;
+    }
+    
+    while ([rootVC isKindOfClass:[UINavigationController class]]) {
+        rootVC = [(UINavigationController *)rootVC topViewController];
+    }
+    
+    return rootVC;
+}
+
+//激励视频渲染完成并展示
+- (void)nativeExpressRewardedVideoAdViewRenderSuccess:(BUNativeExpressRewardedVideoAd *)rewardedVideoAd {
+    [self.rewardedAd showAdFromRootViewController: [self rootViewController]];
+    
+}
+
+//激励视频播放完成
+- (void)nativeExpressRewardedVideoAdDidPlayFinish:(BUNativeExpressRewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *_Nullable)error {
+    NSMutableDictionary *mutableDictionary=[NSMutableDictionary dictionaryWithCapacity:3];
+    [mutableDictionary setValue:@YES forKey:@"rewardVerify"];
+    [mutableDictionary setValue:NULL forKey:@"rewardAmount"];
+    [mutableDictionary setValue:NULL forKey:@"rewardName"];
+    
+    [globalMethodChannel invokeMethod:@"onRewardResponse" arguments:mutableDictionary];
+}
+
+- (void)nativeExpressRewardedVideoAdServerRewardDidSucceed:(BUNativeExpressRewardedVideoAd *)rewardedVideoAd verify:(BOOL)verify {
+    
+}
+
+//激励视频关闭
+- (void)nativeExpressRewardedVideoAdDidClose:(BUNativeExpressRewardedVideoAd *)rewardedVideoAd {
+    
 }
 
 @end
